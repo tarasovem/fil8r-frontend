3172bdeb114540f85dd8262cb16b39ea0ea1b0e8
Evgeniy Tarasov <tarasovem@gmail.com>
2021-06-13 03:08:13 +0300

    Выпиливаем старую таблицу

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material/material.module";
-import { DataTableComponent } from './data-table/data-table.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataTableComponent,
     FileUploadComponent
   ],
   imports: [
